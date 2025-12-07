8943d506b6511d8e7a26f7a6fb5a5df26d49b505
Masayoshi Wada <developer@andantesoftware.com>
2025-12-07 12:29:01 +0900

    add Open method

--- internal/browser/opener.go
+++ internal/browser/opener.go
@@ -1,9 +1,16 @@
 package browser
 
+import "os/exec"
+
 type Opener struct {
 	command string
 }
 
 func NewOpener(command string) *Opener {
 	return &Opener{command: command}
+}
+
+func (o *Opener) Open(filePath string) error {
+	cmd := exec.Command(o.command, filePath)
+	return cmd.Run()
 }
--- internal/browser/opener_test.go
+++ internal/browser/opener_test.go
@@ -8,3 +8,19 @@ func TestNewOpener(t *testing.T) {
 		t.Errorf("NewOpener().command = %q, want %q", opener.command, "firefox")
 	}
 }
+
+func TestOpen(t *testing.T) {
+	opener := NewOpener("echo")
+	err := opener.Open("/path/to/file.html")
+	if err != nil {
+		t.Errorf("Open() error: %v", err)
+	}
+}
+
+func TestOpen_InvalidCommand(t *testing.T) {
+	opener := NewOpener("nonexistent-command-12345")
+	err := opener.Open("/path/to/file.html")
+	if err == nil {
+		t.Error("Open() should return error for invalid command")
+	}
+}
